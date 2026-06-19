5f9e3e14f791ef4dd854792997b876a6333d909f
selinyi123 <100994333+selinyi123@users.noreply.github.com>
2026-06-20 00:15:39 +0800

    export ascii and braille enhancement APIs

--- src/braille_dotmatrix_engine/__init__.py
+++ src/braille_dotmatrix_engine/__init__.py
@@ -1,6 +1,8 @@
 from .config import BrailleArtConfig, MaterialProfile, PrinterProfile, TactileGeometry
 from .pipeline import create_demo_image, process_image
 from .chromatic import build_chromatic_array, render_chromatic_png
+from .ascii_backend import render_ascii_text, write_ascii_output
+from .braille_enhance import enhance_sampled_values
 from .braille_unicode import (
     BRAILLE_BASE,
     BRAILLE_BIT_LAYOUT,
@@ -23,6 +25,9 @@ __all__ = [
     'process_image',
     'build_chromatic_array',
     'render_chromatic_png',
+    'render_ascii_text',
+    'write_ascii_output',
+    'enhance_sampled_values',
     'encode_braille_cell',
     'decode_braille_cell',
     'encode_to_braille_matrix',
