39fef93ef60e0bb9dd300ff98d4fa7b70f191a49
selinyi123 <100994333+selinyi123@users.noreply.github.com>
2026-06-21 13:56:12 +0800

    Add version constants regression test

---
+++ tests/test_version_constants.py
@@ -0,0 +1,7 @@
+from braille_dotmatrix_engine.schema import BENCHMARK_SCHEMA_VERSION, PACKAGE_VERSION, RENDER_SCHEMA_VERSION
+
+
+def test_version_constants_for_current_release():
+    assert PACKAGE_VERSION == '1.16.0'
+    assert RENDER_SCHEMA_VERSION == '1.11'
+    assert BENCHMARK_SCHEMA_VERSION == '1.11'
